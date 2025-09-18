42f329093cf1ee0c020f7230243daab362d0edba
Areeba Khan <Saifkhandaha132@gmail.com>
2025-09-18 21:08:07 +0500

    javascript
    nice big

---
+++ Project2_with_js/main.js
@@ -0,0 +1,19 @@
+//Artimetic operators
+// let a = 5;
+// let b = 2;
+// console.log("a + b =",a + b );
+// console.log("a - b =",a - b );
+// console.log("a / b =",a / b );
+// console.log("a % b =",a % b );
+// console.log("a * b =",a * b );
+// console.log("a ** b =",a ** b );
+
+//Unary operator
+let a = 5;
+let b = 2;
+
+console.log("a =", a, " & b =", b);
+--a
+console.log("--a =", a++); //8
+
+console.log("b =", b);
