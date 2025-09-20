f731c1afd91aa6dd9739047b3ddd67fd4e373945
Areeba Khan <Saifkhandaha132@gmail.com>
2025-09-20 21:02:47 +0500

    Hi wah very good
    Bohat Barya

---
+++ Project4_with_js/README.md
@@ -0,0 +1,2 @@
+Areeba Abid 
+Javadcript
---
+++ Project4_with_js/main.js
@@ -0,0 +1,11 @@
+//Condition Statements
+
+let age = 18;
+let mode = "dark";
+
+if(mode === "dark") {
+    color = "black";
+} else {
+ color = "white";
+}
+console.log(color);
