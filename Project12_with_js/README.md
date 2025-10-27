3e0ed7bb32f1fe5ce85ee39ab1f4b2b58aceb0d9
Areeba Khan <Saifkhandaha132@gmail.com>
2025-10-27 21:20:16 +0500

    Good

---
+++ Project12_with_js/README.md
@@ -0,0 +1,2 @@
+Areeba Abid
+Github
---
+++ Project12_with_js/main.js
@@ -0,0 +1,20 @@
+//let marks = [48,38,48,75,];
+//console.log(marks);
+//console.log(marks.lenth); 
+
+//heroes = ["sarim", "Ali", "Sami"];
+//console.log(heroes);//
+
+// using a for loop
+//let heroes = ["Ali","Ali","Cami","Ali"];
+
+//for(let i = 0; i < heroes.length; i++) {
+   // console.log(heroes[i]);
+//}
+//for of
+//for(let heroe of heroes) {
+    //console.log(heroe);
+//}
+
+
+ 
