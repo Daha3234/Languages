f2063bbeb5855f36e04329b2265f4138ccb81629
Areeba Khan <Saifkhandaha132@gmail.com>
2025-11-14 21:15:43 +0500

    Excellent

---
+++ Project js_with_14/main.js
@@ -0,0 +1,34 @@
+// // //sum function
+// // function sum(a,b){
+// //     return a+b;
+// // }
+
+// // //multiplication function 
+// // function mul(a,b) {
+// //     return a * b;
+// // }
+// // const x = 5;
+
+// // const arrowSum =(a, b) => {
+// //     console.log(a+b);
+// // }
+
+// // const arrowMul = (a, b) => {
+// //     console.log(a*b);
+// // }
+
+
+// countVowels
+// function countVowels(str) {
+//     //"ApnaCollege",count = 5
+//     for(const char of str) {
+//         console.log(char);
+//     }
+// }
+
+
+
+
+
+
+
