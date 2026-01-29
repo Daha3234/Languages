c4ff4e6af9a21843726064834b795480ce65dd1e
Areeba Khan <Saifkhandaha132@gmail.com>
2026-01-29 22:01:12 +0500

    Loops

---
+++ Project js_with_25/main.js
@@ -0,0 +1,15 @@
+// for(let a=1; a <= 2; a++) {
+//     console.log("Areeba");
+// }
+  
+// let sum = 0 
+// for(a = 1; a <= 6; a++){
+//     sum = sum + a;
+// }
+// console.log("sum =", sum);
+
+for(let num = 1; num <= 100; num++){
+    if(num3 === 0){
+        console.log("num" , num);
+    }
+}
