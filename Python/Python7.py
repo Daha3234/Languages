776b8a6652b522b5a67c78f867f5a196d51578cd
Areeba Khan <areebacode1@gmail.com>
2026-07-05 22:37:37 +0500

    Sets Method👍
    😚😎😊

--- Python/Python7.py
+++ Python/Python7.py
@@ -8,3 +8,40 @@ print(len(collection))
 collection1 = set()
 print(type(collection1))
 
+#method of set
+#Add method
+
+collection.add(1)
+collection.add(2)
+collection.add(2)
+
+
+
+#Set remove(el)
+
+#Set Clear method
+collection.add(1)
+collection.add(2)
+collection.add("apnacollege")
+collection.add((1, 2, 3,))
+
+collection.clear()
+print(len(collection))
+
+#Set pop method
+collection = {"hello" , "coding", "coding", "python"}
+print(collection.pop())
+print(collection.pop())
+
+#Set union methon
+set1 = {1,2,3}
+set2 = {2,3,4}
+print(set1.union(set2))
+print(set1)
+print(set2)
+
+#Set intersection method
+
+set1 = {1, 2, 3}
+set2 = {1, 3, 5, 2}
+print(set1.intersection(set2))
---
+++ Python/Python8.py
@@ -0,0 +1,17 @@
+#Practice questions 1
+
+Dictionary = {
+    "cat" : "a samall animal" ,
+    "table" : "a piece of furniture"
+        
+}
+print(Dictionary["cat"])
+
+#Practice question 2
+
+set1 = {
+    "python", "java", "c++", "javascript","java",
+    "python", "c++", "C"}
+print(len(set1))
+
+
