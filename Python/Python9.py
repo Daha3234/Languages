774072ed02081a0e30fa9c1deaf34c8e86f9c63b
Areeba Khan <areebacode1@gmail.com>
2026-07-06 21:25:01 +0500

    While loops
    👍👍👍

---
+++ Python/Python9.py
@@ -0,0 +1,13 @@
+#Loops in Python
+#While loops
+i = 1
+while i <= 100:
+    print("1,2,3,4,5,6,7,8,9,10" , i)
+    i+=1
+
+
+    e = 1
+    while e > 5:
+        print(i)
+        e -= 1
+        print("loops ended")
