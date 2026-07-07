7766a7ae87198f9196282a72bc0934875e950a62
Areeba Khan <areebacode1@gmail.com>
2026-07-07 22:21:37 +0500

    Loops For
    👍😎👌👏

---
+++ Python/Python12.py
@@ -0,0 +1,9 @@
+#Loops in Python
+str = "AreebaAbid"
+ 
+for T in str:
+    if(T == "i"):
+        break
+    print(T)
+else:
+    print("The end")
---
+++ Python/Python13.py
@@ -0,0 +1,8 @@
+#Practice Questions
+nums = [1,4,9,25,36,49,64,64,81,100]
+for nums in nums:
+    print(nums)
+
+    #Q#3
+
+    
