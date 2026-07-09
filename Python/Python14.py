3cf6e03c82b461dc760e87b7b607f4e3e869dd5f
Areeba Khan <areebacode1@gmail.com>
2026-07-09 22:50:44 +0500

    Range in Python
    range is very simple logic

---
+++ Python/Python14.py
@@ -0,0 +1,26 @@
+#Range() 
+seq = range(10)
+
+for e in range(10):
+    print(e)
+
+
+
+#Let Practice questions
+
+for f in range(1, 101):
+    print(f)
+
+
+#Let Practice question
+
+for g in range(100, 0, -1):
+    print(g)
+
+
+    #Let practice
+
+    n = int(input("enter a number:"))
+
+    for h in range(1, 11):
+        print(n * h)
